1e12d9444e72cd791d1424993d5258fe9b17da02
kshku <1211shree@gmail.com>
2024-05-19 12:08:06 +0530

    Refractored the test file

--- my_test.c
+++ my_test.c
@@ -3,9 +3,13 @@
 
 #include "wallhavenapi.h"
 
-// NOTE: Don't uncomment everything at once
-
 // To test whether things works or not
+
+void print_and_reset(Response *response)
+{
+    printf("%s\n", response->value);
+    response->size = 0;
+}
 
 int main()
 {
@@ -13,79 +17,70 @@ int main()
 #ifdef APIKEY
     wallhaven_apikey(wa, APIKEY);
 #endif
-    /*
-    Response r;
-    wallhaven_write_to_response(wa, &r);
-    // wallhaven_my_collections(wa);
-    // wallhaven_collections_of(wa, "Yukinoshita/854694");
-    // wallhaven_wallpapers_of_collections(wa, "Yukinoshita", "854694", 0);
-    printf("%s\n", r.value);
-    */
+    Response response = {0};
+    wallhaven_write_to_response(wa, &response);
 
-    // /*
-    Response r;
-    wallhaven_write_to_response(wa, &r);
+    wallhaven_my_collections(wa);
+    print_and_reset(&response);
+
+    wallhaven_collections_of(wa, "Yukinoshita");
+    print_and_reset(&response);
+
+    // Just hack
+    wallhaven_collections_of(wa, "Yukinoshita/854694");
+    print_and_reset(&response);
+
+    wallhaven_wallpapers_of_collections(wa, "Yukinoshita", "854694", 0);
+    print_and_reset(&response);
+
     for (int i = 0; i < 50; ++i)
     {
         printf("Iteration %d\n", i + 1);
         wallhaven_wallpaper_info(wa, "3lepy9");
-        r.size = 0;
-        printf("\n");
+        print_and_reset(&response);
     }
-    free(r.value);
-    // */
 
-    /*
     // Wallpaper info
-    FILE *file = fopen("./test.json", "w");
-    if (!(file))
+    FILE *test = fopen("./test.json", "w");
+    if (!(test))
         return 1;
-    Response r
-    wallhaven_write_to_response(wa, &r);
     WallhavenCode c = wallhaven_wallpaper_info(wa, "3lepy9");
-    wallhaven_write_to_file(wa, file);
-    // printf("%s\n", r.value);
+    print_and_reset(&response);
+    wallhaven_write_to_file(wa, test);
     wallhaven_wallpaper_info(wa, "3lepy9");
-    free(r.value);
-    fclose(file);
+    fclose(test);
 
     // Tag info
-    file = fopen("./test1.json", "w");
-    if (!(file))
+    FILE *test1 = fopen("./test1.json", "w");
+    if (!(test1))
         return 2;
-    Response rt;
-    wallhaven_write_to_response(wa, &rt);
     WallhavenCode ct = wallhaven_tag_info(wa, "77513");
-    // printf("%s\n", rt.value);
-    wallhaven_write_to_file(wa, file);
+    print_and_reset(&response);
+    wallhaven_write_to_file(wa, test1);
     wallhaven_tag_info(wa, "77513");
-    free(rt.value);
-    fclose(file);
+    fclose(test1);
 
     // User settings
-    file = fopen("./test2.json", "w");
-    if (!(file))
+    FILE *test2 = fopen("./test2.json", "w");
+    if (!(test2))
         return 3;
-    Response rs;
-    wallhaven_write_to_response(wa, &rs);
     WallhavenCode cs = wallhaven_user_settings(wa);
     if (cs == WALLHAVEN_OK)
     {
-        // printf("%s\n", rs.value);
-        wallhaven_write_to_file(wa, file);
+        print_and_reset(&response);
+        wallhaven_write_to_file(wa, test2);
         wallhaven_user_settings(wa);
     }
     else
     {
         printf("No API key is provided\n");
     }
-    free(rs.value);
-    fclose(file);
+    fclose(test2);
 
     // Search
     Parameters p = (Parameters){
         .q = &(Query){
-            0
+            0,
             // .tags = "Tag -Exclude +Include",
             // .user_name = "user name",
             // .id = "id",
@@ -105,8 +100,10 @@ int main()
         // .seed = "azAZ09"
     };
 
-    file = fopen("./test3.json", "w");
-    wallhaven_write_to_file(wa, file);
+    FILE *test3 = fopen("./test3.json", "w");
+    if (!test3)
+        return 4;
+    wallhaven_write_to_file(wa, test3);
     WallhavenCode wc = wallhaven_search(wa, &p);
     if (wc == WALLHAVEN_NO_API_KEY)
         printf("Api key is not given\n");
@@ -114,8 +111,8 @@ int main()
         printf("Used id in a combination\n");
     else if (wc == WALLHAVEN_SORTING_SHOULD_BE_TOPLIST)
         printf("Sorting is not toplist\n");
-    fclose(file);
-    */
+    fclose(test3);
 
     wallhaven_free(wa);
+    free(response.value);
 }
